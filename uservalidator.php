38d77569ad089a9b4f0e22331f4fbf3003a9665b
0x5FE <65371336+0x5FE@users.noreply.github.com>
2023-10-21 21:22:08 +0000

    Update uservalidator.php

--- uservalidator.php
+++ uservalidator.php
@@ -6,112 +6,98 @@ Version: 1.0
 Author: 0x5FE
 */
 
-
 add_action('login_form', 'validate_user_permission');
 
 function validate_user_permission() {
-    
-if (isset($_POST['log']) && isset($_POST['pwd'])) {
-$username = sanitize_user($_POST['log']);
-$password = $_POST['pwd'];
+    if (isset($_POST['log']) && isset($_POST['pwd'])) {
+        $username = sanitize_user($_POST['log']);
+        $password = sanitize_text_field($_POST['pwd']);
 
-    if (!is_user_valid($username)) {
-        block_login_attempt();
+        if (!is_user_valid($username)) {
+            block_login_attempt('Invalid username.');
+        }
+
+        if (is_brute_force_attempt($username)) {
+            block_login_attempt('Too many login attempts. Please try again later.');
+        }
+
+        if (!is_password_valid($password)) {
+            block_login_attempt('Invalid password.');
+        }
     }
-
-    if (is_brute_force_attempt($username)) {
-        block_login_attempt();
-    }
-
-    if (!is_password_valid($password)) {
-        block_login_attempt();
-    }
-}
-
-
 }
 
 function is_user_valid($username) {
-$user = get_user_by('login', $username);
+    $user = get_user_by('login', $username);
 
-if (!$user) {
-    return false; 
-}
+    if (!$user) {
+        return false;
+    }
 
-$allowed_permission_levels = array('administrator', 'editor', 'author');
-if (!in_array($user->roles[0], $allowed_permission_levels)) {
-    return false; 
-}
+    $allowed_permission_levels = array('administrator', 'editor', 'author');
+    if (!in_array($user->roles[0], $allowed_permission_levels)) {
+        return false;
+    }
 
-return true; 
-
+    return true;
 }
 
 function is_brute_force_attempt($username) {
+    $ip = $_SERVER['REMOTE_ADDR'];
 
-$ip = $_SERVER['REMOTE_ADDR'];
+    $attempts = get_user_meta($username, 'login_attempts', true);
 
-$attempts = get_user_meta($username, 'login_attempts', true);
+    if (!$attempts) {
+        $attempts = array();
+    }
 
-if (!$attempts) {
-    $attempts = array();
-}
+    $max_attempts = 5;
+    $ip_attempts = array_filter($attempts, function ($attempt) use ($ip) {
+        return $attempt['ip'] === $ip;
+    });
 
+    if (count($ip_attempts) >= $max_attempts) {
+        return true;
+    }
 
-$max_attempts = 5; 
-$ip_attempts = array_filter($attempts, function ($attempt) use ($ip) {
-    return $attempt['ip'] === $ip;
-});
+    $attempts[] = array(
+        'ip' => $ip,
+        'timestamp' => time(),
+    );
 
-if (count($ip_attempts) >= $max_attempts) {
-    return true; 
-}
+    update_user_meta($username, 'login_attempts', $attempts);
 
-
-$attempts[] = array(
-    'ip' => $ip,
-    'timestamp' => time(),
-);
-
-
-update_user_meta($username, 'login_attempts', $attempts);
-
-return false; 
-
+    return false;
 }
 
 function is_password_valid($password) {
-
-if (strlen($password) < 8) {
-    return false; 
-}
-
-
-$password_checks = array(
-    '/[A-Z]/', 
-    '/[a-z]/', 
-    '/\d/', 
-    '/[^A-Za-z0-9]/', 
-);
-
-foreach ($password_checks as $check) {
-    if (!preg_match($check, $password)) {
-        return false; 
+    if (strlen($password) < 8) {
+        return false;
     }
+
+    $password_checks = array(
+        '/[A-Z]/', // At least one uppercase letter
+        '/[a-z]/', // At least one lowercase letter
+        '/\d/',    // At least one digit
+        '/[^A-Za-z0-9]/', // At least one special character
+    );
+
+    foreach ($password_checks as $check) {
+        if (!preg_match($check, $password)) {
+            return false;
+        }
+    }
+
+    $common_passwords = array('123456', 'password', 'qwerty');
+    if (in_array($password, $common_passwords)) {
+        return false;
+    }
+
+    return true;
 }
 
-
-$common_passwords = array('123456', 'password', 'qwerty');
-if (in_array($password, $common_passwords)) {
-    return false; /
-}
-
-return true; 
-
-}
-
-function block_login_attempt() {
-$login_url = wp_login_url();
-wp_redirect(add_query_arg('login_error', '1', $login_url));
-exit;
+function block_login_attempt($error_message) {
+    $login_url = wp_login_url();
+    wp_redirect(add_query_arg('login_error', urlencode($error_message), $login_url));
+    exit;
 }
